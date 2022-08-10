7557ccfd5aca79e9409bbc06d2ed2285d14597ed
Alexander Efimov <a.efimov@inenergy.ru>
2022-08-10 15:48:58 +0300

    fix installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,9 +5,8 @@ npm i
 npm run build
 
 # MAIN APP AUTOSTART
-chmod +x dist/hydrogen-energy-ui*.AppImage
+chmod +x dist/redox-ui*.AppImage
 mkdir -p ~/inenergy-gui/dist
-cp dist/hydrogen-energy-ui*.AppImage ~/inenergy-gui/dist/
-mkdir ~/.inenergy
-echo '~/inengergy-gui/dist/hydrogen-energy-ui*.AppImage > ~/.inenergy/hydrogen-energy-ui.log' > ~/.config/openbox/autostart
-echo '{"CRITICAL_CONCENTRATION": 75}' > ~/.inenergy/config.json
+cp dist/redox-ui*.AppImage ~/inenergy-gui/dist/
+mkdir -p ~/.inenergy
+echo '~/inengergy-gui/dist/redox-ui*.AppImage > ~/.inenergy/hydrogen-energy-ui.log' > ~/.config/openbox/autostart
